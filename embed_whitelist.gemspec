8b53063650d58a6b740c6f7ff36bfd919b7d63d0
Richard Luther <richard.luther@gmail.com>
2009-02-25 14:29:44 -0800

    first commit

---
+++ embed_whitelist.gemspec
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |s|
+  s.name = %q{embed_whitelist}
+  s.version = "0.0.1"
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.authors = ["Richard Luther"]
+  s.date = %q{2009-02-25}
+  s.description = %q{Simple plugin for sanitizing embed widgets. Uses a white list approach for tags, attributes, and domains. I will be adding to the list of allowed domains.}
+  s.email = ["richard@tangofoxtrot.com"]
+  s.extra_rdoc_files = ["History.txt", "Manifest.txt", "README.rdoc"]
+  s.files = ["History.txt", "Manifest.txt", "README.rdoc", "Rakefile", "lib/embed_whitelist.rb", "lib/embed_whitelistl/embed_whitelist_config.yml", "script/console", "script/destroy", "script/generate", "test/test_embed_whitelist.rb", "test/test_helper.rb"]
+  s.has_rdoc = true
+  s.homepage = %q{FIX (url)}
+  s.rdoc_options = ["--main", "README.rdoc"]
+  s.require_paths = ["lib"]
+  s.rubyforge_project = %q{embed_whitelist}
+  s.rubygems_version = %q{1.3.1}
+  s.summary = %q{Simple plugin for sanitizing embed widgets}
+  s.test_files = ["test/test_embed_whitelist.rb", "test/test_helper.rb"]
+
+  if s.respond_to? :specification_version then
+    current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
+    s.specification_version = 2
+
+    if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
+      s.add_development_dependency(%q<newgem>, [">= 1.2.3"])
+      s.add_development_dependency(%q<hoe>, [">= 1.8.0"])
+    else
+      s.add_dependency(%q<newgem>, [">= 1.2.3"])
+      s.add_dependency(%q<hoe>, [">= 1.8.0"])
+    end
+  else
+    s.add_dependency(%q<newgem>, [">= 1.2.3"])
+    s.add_dependency(%q<hoe>, [">= 1.8.0"])
+  end
+end
---
+++ test/test_embed_whitelist.rb
@@ -0,0 +1,73 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestEmbedWhitelist < Test::Unit::TestCase
+  include EmbedWhitelist
+  
+  def setup
+    @youtube_string = %(<object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/W3f6BOrD9Ek&hl=en&fs=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/W3f6BOrD9Ek&hl=en&fs=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed></object>)
+    @valid_youtube_doc = Hpricot(@youtube_string, :fixup_tags => true)
+  end
+  
+  def test_domain_allowed
+    assert domain_allowed?("youtube.com"), "Youtube should be in the allowed list"
+    assert !domain_allowed?("evilsite.com"), "Evilsite should be in the allowed list"    
+  end
+  
+  def test_attr_allowed
+    assert attr_allowed?("embed", "type"), "Embed should allow the type attr"
+    assert !attr_allowed?("embed", "onclick"), "Embed should not allow the onclick attr"    
+  end
+
+  def test_attr_allowed_and_capitialized_attrs
+    assert attr_allowed?("embed", "TYPE"), "Embed should allow the type attr"
+    assert !attr_allowed?("embed", "onClick"), "Embed should not allow the onclick attr"    
+  end
+  
+  def test_open_embed
+    assert open_embed(@youtube_string).is_a?(Hpricot), "I am not an hpricot"
+  end
+  
+  def test_check_embed_for_valid_tags
+    open_embed(@youtube_string)
+    assert_equal check_embed_for_valid_tags.to_html, @valid_youtube_doc.to_html
+  end
+
+  def test_check_embed_for_valid_tags_with_invalid_a
+    open_embed(@youtube_string + "<a href='javscript:alert(blah)'>Click ME</a>")
+    assert_equal check_embed_for_valid_tags.to_html, @valid_youtube_doc.to_html
+  end
+
+  def test_check_embed_for_valid_attrs
+    open_embed(@youtube_string)
+    assert_equal check_embed_for_valid_attrs.to_html, @valid_youtube_doc.to_html    
+  end
+
+  def test_check_embed_for_valid_attrs_with_invalid_onclick
+    open_embed(@youtube_string)
+    assert_equal check_embed_for_valid_attrs.to_html, @valid_youtube_doc.to_html 
+  end
+
+  def test_check_src_domain
+    assert check_src_domain("http://www.youtube.com/v/W3f6BOrD9Ek&hl=en&fs=1")
+  end
+
+  def test_check_src_domain
+    assert_raise(EmbedWhitelist::DomainError) { check_src_domain("http://www.evilyoutube.com/v/W3f6BOrD9Ek&hl=en&fs=1") }
+  end
+
+  def test_check_embed_for_valid_attrs_with_evil_url
+    open_embed(@youtube_string.gsub(/youtube/, "evilwebsite"))
+    assert_raise(EmbedWhitelist::DomainError) { check_embed_for_valid_attrs.to_html}  
+  end
+
+  def test_parse_embed
+    embed_string = parse_embed(@youtube_string)
+    assert_equal embed_string, @valid_youtube_doc.to_html 
+  end
+
+  def test_parse_embed_with_bad_tags
+    embed_string = parse_embed(@youtube_string + "<a href='javscript:alert(blah)'>Click ME</a>")
+    assert_equal embed_string, @valid_youtube_doc.to_html 
+  end
+  
+end
